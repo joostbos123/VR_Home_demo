4a037259f1a52dc8301eadf265a1f73cd7604945
Bos, Joost <joost.bos@hotmail.com>
2022-06-27 10:23:39 +0200

    Allow more distance from coordinates

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 window.onload = function () {
 
     console.log('test')
+
+    const distanceMsg = document.querySelector('[gps-entity-place]').getAttribute('distanceMsg');
+    console.log(distanceMsg);   // "890 meters"
 }
